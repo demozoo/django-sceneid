82c843d0afd42260dac6a109e9b56bdcb8ce8122
Matt Westcott <matt@west.co.tt>
2021-05-07 22:23:16 +0100

    Implement sceneid handshake on login view

--- sceneid/views.py
+++ sceneid/views.py
@@ -1,4 +1,6 @@
 from django.conf import settings
+from django.core.exceptions import SuspiciousOperation
+from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.urls import reverse
 from django.utils.crypto import get_random_string
@@ -14,15 +16,36 @@ def _get_sceneid_client():
     )
 
 
+def _get_return_uri():
+    return settings.BASE_URL + reverse('sceneid:login')
+
+
 def auth_redirect(request):
+    """
+    Generate the SceneID auth redirect URL and send user there.
+    """
     client = _get_sceneid_client()
-    return_uri = settings.BASE_URL + reverse('sceneid:login')
     state = get_random_string(length=32)
 
-    redirect_uri = client.get_authorization_uri(state, return_uri)
+    redirect_uri = client.get_authorization_uri(state, _get_return_uri())
     request.session['sceneid_state'] = state
     return redirect(redirect_uri)
 
 
 def login(request):
-    pass
+    """
+    Process the SceneID Oauth response
+    """
+    state = request.GET['state']
+    code = request.GET['code']
+
+    if (state != request.session['sceneid_state']):
+        raise SuspiciousOperation("State mismatch!")
+
+    client = _get_sceneid_client()
+    token_data = client.get_access_token(code, _get_return_uri())
+    access_token = token_data['access_token']
+    request.session['sceneid_accesstoken'] = access_token
+    user_data = client.get_user_data(access_token)
+
+    return HttpResponse(repr(user_data))
