0f51fa7188fe9890300f404f00a30dfb6a3f447d
Matt Westcott <matt@west.co.tt>
2022-07-13 10:40:46 +0100

    Version bump to 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="django-sceneid",
-    version="0.1",
+    version="0.1.1",
     packages=find_packages(exclude=('tests',)),
     include_package_data=True,
     test_suite="tests",
